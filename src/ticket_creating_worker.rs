7f934af34f16d50415d76cc7ad4d5754fe5c1ac3
seprov <provenzano.sebastian@gmail.com>
2024-08-26 17:16:57 -0500

    this works!

--- src/ticket_creating_worker.rs
+++ src/ticket_creating_worker.rs
@@ -1,31 +1,35 @@
 use std::io::{self, ErrorKind, Read};
 
-use crate::{input_getter::get_input, schedule_state::ScheduleState::Idea, ticket::Ticket, ticket_serializer};
+use crate::{
+  input_getter::get_input, schedule_state::ScheduleState::Idea, ticket::Ticket, ticket_serializer,
+};
 
 pub fn create_ticket() -> Result<Ticket, io::Error> {
   println!("let's create a ticket!");
   println!("what ticket id do you want? enter up to 8 1-byte characters");
   let binding = get_input()?;
   let input = binding.trim();
-  if (&input).len() < 8 {
-    for byte in input.as_bytes() {
-      println!("{}",byte)
+  if (&input).len() <= 8 {
+    let mut buffer = [0u8; 8];
+    let id_bytes = input.as_bytes();
+    for (i, &b) in id_bytes.iter().enumerate().take(8) {
+      buffer[8 - (i + 1)] = b;
     }
-    let id_bytes = (input).as_bytes().try_into();
-    match id_bytes {
-      Ok(bytes) => {
-        let ticket = Ticket { id: bytes, schedule_state: Idea };
-        println!("creating ticket");
-        let file_path = format!("data/tickets/{}.json", input);
-        ticket_serializer::serialize(&file_path, &ticket);
-        Ok(ticket)
-      }
-      Err(e) => Err(io::Error::new(ErrorKind::InvalidInput, (format!("we failed! {} {}",e,input)))),
-    }
+
+    let ticket = Ticket {
+      id: buffer,
+      schedule_state: Idea,
+    };
+
+    println!("creating ticket");
+    let file_path = format!("data/tickets/{}.json", input);
+    ticket_serializer::serialize(&file_path, &ticket);
+
+    Ok(ticket)
   } else {
     Err(io::Error::new(
       ErrorKind::InvalidInput,
-      "wrong number of chars!",
+      "too many chars!",
     ))
   }
 }
