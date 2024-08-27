b48f55cbb8b9036a143ecba2ae81e2503daa0b3f
seprov <provenzano.sebastian@gmail.com>
2024-08-27 18:45:27 -0500

    change style

--- src/schedule_state_provider.rs
+++ src/schedule_state_provider.rs
@@ -1,4 +1,4 @@
-pub const SCHEDULE_STATES: [&'static str; 4] = ["Idea", "Defined", "InProgress", "Released"];
+pub const SCHEDULE_STATES: [&'static str; 4] = ["Idea", "Defined", "In-progress", "Released"];
 
 pub fn get_schedule_states() -> Vec<String> {
   SCHEDULE_STATES.map(|x| x.to_owned()).to_vec()
