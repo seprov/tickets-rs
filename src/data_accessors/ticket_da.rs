cbf70a0fb61a52a7d4f915d3889201a94e421f97
seprov <provenzano.sebastian@gmail.com>
2024-09-02 16:49:29 -0500

    some cleanup

--- src/data_accessors/ticket_da.rs
+++ src/data_accessors/ticket_da.rs
@@ -4,6 +4,8 @@ use crate::{data_accessors::path_provider, models::{ticket::Ticket, ticket_id::T
 
 use super::json_ticket_serializer;
 
+// currently using json implementation
+
 pub fn save_ticket(x: &Ticket) -> Result<(), Box<dyn Error>> {
   let file_path = path_provider::get_ticket_path(&x.id);
   json_ticket_serializer::serialize(&file_path, x)
--- src/workers/stdin_ticket_creating_worker.rs
+++ src/workers/stdin_ticket_creating_worker.rs
@@ -15,20 +15,19 @@ impl TicketCreatingWorker for StdinTicketCreatingWorker {
     println!("please enter a ticket id for your new ticket");
     // stdin specific user input for getting ticket id
     let ticket_id = stdin_ticket_id_getter::get_ticket_id()?;
+
     let id_string = &ticket_id.to_string();
 
     // ticket json datastore specific ticket already exists validation
     json_ticket_id_validator::ticket_already_exists(&ticket_id)?;
 
-    // ticket json datastore specific ticket saving
-    let file_path = crate::data_accessors::path_provider::get_ticket_path(&ticket_id);
-
     let ticket = Ticket::new(ticket_id, "Idea".to_owned());
 
     // stdout specific view
     println!("creating ticket");
-    // ticket json datastore specific ticket saving
+
     ticket_da::save_ticket(&ticket)?;
+    
     // stdout specific view
     println!("created ticket: {}", id_string);
 
