1473dfc9b081cf9595aa9759d04a030a56c26a6b
seprov <provenzano.sebastian@gmail.com>
2024-09-02 20:50:26 -0500

    that was easy

--- src/view_providers/stdout_ticket_list_view_provider.rs
+++ src/view_providers/stdout_ticket_list_view_provider.rs
@@ -3,8 +3,8 @@ use super::ticket_list_view_provider::TicketListViewProvider;
 pub struct StdoutTicketListViewProvider;
 
 impl TicketListViewProvider for StdoutTicketListViewProvider {
-  println!("\nyour tickets are:");
   fn display_tickets(&self, tickets: &[crate::models::ticket::Ticket]) {
+    println!("\nyour tickets are:");
     tickets
       .iter()
       .for_each(|x| println!("  {}", x.id.to_string()));
